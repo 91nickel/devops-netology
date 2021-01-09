55b1a51edf6205492449990ba17c3d5fc6a3edf3
nikita <nikita@foroffice.ru>
2021-01-09 20:32:32 +0000

    Задание 7.2 облачные провайдеры и синтаксис терраформ

---
+++ terraform/main.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
+
+data "aws_instance" "web" {}
+
+resource "aws_instance" "web" {
+  count = 1
+  ami = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  cpu_core_count = 1
+  cpu_threads_per_core = 1
+  tags = {
+    Name = "HelloNetology"
+  }
+}
